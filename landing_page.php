a3aec11027a1e9ecce4e4ec9bfcc24a81ce8b094
Avinash <avinashnm07@gmail.com>
2023-12-12 23:43:23 +0530

    added a navigation bar to the landing page

---
+++ landing_page.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="Bootstrap/CSS/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="Bootstrap/JS/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="landing_page1.css">
+</head>
+<body>
+  <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <img src="skillup_logo1.png" class="nav_logo"/>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <ul class="nav nav-underline">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Business Administration</a></li>
+  <li><a class="dropdown-item" href="#">Computer Science</a></li>
+  <li><a class="dropdown-item" href="#">Economics</a></li>
+  <li><a class="dropdown-item" href="#">English Literature</a></li>
+  <li><a class="dropdown-item" href="#">History</a></li>
+  <li><a class="dropdown-item" href="#">Mathematics</a></li>
+  <li><a class="dropdown-item" href="#">Chemistry</a></li>
+  <li><a class="dropdown-item" href="#">Physics</a></li>
+  
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Separated link</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link login-link" href="sign_in.html">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="new_user.html"><button class="signup_btn">Sign up</button></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="About.html">About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</body>
+</html>
